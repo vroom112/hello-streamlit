6b0e39e0a50bb23a9f9258d59c3f5d216fc33420
vroom112 <150968497+vroom112@users.noreply.github.com>
2023-11-29 13:17:29 +0200

    Update project.py

--- Hello.py
+++ Hello.py
@@ -21,7 +21,7 @@ LOGGER = get_logger(__name__)
 def run():
     st.set_page_config(
         page_title="Hello",
-        page_icon="👋",
+        page_icon="😂",
     )
 
     st.write("# Welcome to Streamlit! 👋")
